265df2915d8c3656174e1edd4d019ccb35fdbaf8
Peter  Robinson <drinkcoffee@eml.cc>
2019-12-19 15:53:56 +1000

    Get key now working
    Signed-off-by: Peter  Robinson <drinkcoffee@eml.cc>

--- common/generatewrappers.sh
+++ common/generatewrappers.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 rm -rf build
-solc ../crosschain/common-solidity/coordination-contract/contracts/CrosschainCoordinationV1.sol --bin --abi --optimize -o build
-solc ../crosschain/common-solidity/coordination-contract/contracts/VotingAlgMajorityWhoVoted.sol --bin --abi --optimize -o build
+solc ../common-solidity/coordination-contract/contracts/CrosschainCoordinationV1.sol --bin --abi --optimize -o build
+solc ../common-solidity/coordination-contract/contracts/VotingAlgMajorityWhoVoted.sol --bin --abi --optimize -o build
 ls -al build
 
 # WEB3J=web3j
-WEB3J=../../sidechains-web3j/besucodegen/build/distributions/besucodegen-4.6.0-SNAPSHOT/bin/besucodegen
+WEB3J=../../sidechains-web3j/besucodegen/build/install/besucodegen/bin/besucodegen
 
 
 $WEB3J solidity generate -a=build/CrosschainCoordinationV1.abi -b=build/CrosschainCoordinationV1.bin -o=build -p=tech.pegasys.samples.sidechains.common.coordination.soliditywrappers
