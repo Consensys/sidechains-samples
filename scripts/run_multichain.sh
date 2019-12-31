1618dc49a9d208bf9505a11ec82bea0a867cce9f
Horacio Mijail Antón Quiles <4139546+hmijail@users.noreply.github.com>
2019-12-31 12:09:01 +1000

    Remove crosschain-config (#22)
    * Remove static configuration of crosschain contact info
    
    Signed-off-by: Horacio Mijail Anton Quiles <hmijail@gmail.com>
    
    * Small fix to run_multichain
    
    Signed-off-by: Horacio Mijail Anton Quiles <hmijail@gmail.com>

--- scripts/run_multichain.sh
+++ scripts/run_multichain.sh
@@ -18,9 +18,9 @@ do
     if (( $IS_FIRST == 1))
     then
       IS_FIRST=0
+      tmux start-server \; set -g remain-on-exit on
       tmux new-session -s multichain -d "bash -c \"echo $STANDOUT Running '$FULL_CMD_LINE' $OFFSTANDOUT ; $FULL_CMD_LINE\""
       tmux bind-key -n Escape kill-session
-      tmux set remain-on-exit on
       tmux set -g mouse on
     else
       tmux split-window -b -t 0 -d "bash -c \"echo $STANDOUT Running '$FULL_CMD_LINE' $OFFSTANDOUT ; $FULL_CMD_LINE\""
