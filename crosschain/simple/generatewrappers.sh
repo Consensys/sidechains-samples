a6874a82f81d645f92c49408264b6ffc9dc4f9cc
Horacio Mijail Anton Quiles <hmijail@gmail.com>
2020-01-20 15:44:53 +1000

    Infrastructure improvements
    Signed-off-by: Horacio Mijail Anton Quiles <hmijail@gmail.com>

--- crosschain/simple/generatewrappers.sh
+++ crosschain/simple/generatewrappers.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
+
+# abort if anything fails
+set -e
+set -o pipefail
+
 rm -rf build
 HERE=crosschain/simple
 BUILDDIR=$HERE/build
 CONTRACTSDIR=$HERE/contracts
 PACKAGE=tech.pegasys.samples.crosschain.simple.soliditywrappers
 
-cd ../..
+cd ../.. # we change dirs so the allow-paths parameter can reach common-solidity/ ; it has restrictions/bugs on relative dirs.
 
 # compiling one file also compiles its dependendencies. We use overwrite to avoid the related warnings.
 solc $CONTRACTSDIR/Sc1Contract1.sol --allow-paths . --bin --abi --optimize -o $BUILDDIR --overwrite
 solc $CONTRACTSDIR/Sc2Contract2.sol --allow-paths . --bin --abi --optimize -o $BUILDDIR --overwrite
 
-ls -al $BUILDDIR
+#ls -al $BUILDDIR
 
 WEB3J=../sidechains-web3j/besucodegen/build/install/besucodegen/bin/besucodegen
 
@@ -20,4 +25,4 @@ $WEB3J solidity generate -cc -a=$BUILDDIR/Sc2Contract2.abi -b=$BUILDDIR/Sc2Contr
 
 cd $HERE
 
-ls -al build/tech/pegasys/samples/crosschain/simple/soliditywrappers
+#ls -al build/tech/pegasys/samples/crosschain/simple/soliditywrappers
